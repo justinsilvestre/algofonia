d2bcdbf6fd8299cc49b3c34456d58ec8c64e12db
Justin Silvestre <justinsilvestre@gmail.com>
2025-12-08 10:09:30 +0100

    Fix compile

--- app/listen/channels/droneChord.ts
+++ app/listen/channels/droneChord.ts
@@ -1,7 +1,7 @@
 import * as Tone from "tone";
 import { Chord, Scale, Key, Progression } from "tonal";
 
-import { createChannel } from "../tone";
+import { createChannel, getToneControls } from "../tone";
 import { PulseSynth } from "./../synth/pulseSynth";
 
 export type ToneControls = ReturnType<typeof getToneControls>;
@@ -19,33 +19,35 @@ export const droneChord = createChannel({
 
     return { loopIndex, synth, octave };
   },
-  onLoop: ({ key, mode, chordRootScaleDegree, getChord }, channelState, time) => {
+  onLoop: (
+    { key, mode, chordRootScaleDegree, getChord },
+    channelState,
+    time
+  ) => {
     channelState.loopIndex += 1;
 
-    const scale  = `${key}${channelState.octave} ${mode}`;
-    const notes  = Scale.get(scale).notes;
+    const scale = `${key}${channelState.octave} ${mode}`;
+    const notes = Scale.get(scale).notes;
     const offset = channelState.loopIndex % 2; // 0 or 1
 
     channelState.synth.playChord(
-      [notes[0+offset], notes[3+offset], notes[5+offset]], 
-      "1n", 
-      time+0.1
+      [notes[0 + offset], notes[3 + offset], notes[5 + offset]],
+      "1n",
+      time + 0.1
     );
-      
-    
 
     return channelState;
   },
   respond: (tone, { synth }, { frontToBack, around }) => {
-    const gainValue = (frontToBack / 100 * 14) + 2;
+    const gainValue = (frontToBack / 100) * 14 + 2;
 
-    synth.gainLFOLFO.set({ max: gainValue});
+    synth.gainLFOLFO.set({ max: gainValue });
 
     // const frequency = (around / 100 * 16) + 2;
     // synth.filterLFO.set({ frequency: frequency })
 
     // const modulationIndex = (around / 3);
-    // synth.synth1.set({ modulationIndex }); 
+    // synth.synth1.set({ modulationIndex });
 
     // const frequency = (around / 100);
     // synth.reverbLFO.set({ frequency });
